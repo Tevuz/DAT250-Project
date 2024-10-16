84841104180a498676f2d7a0a1500704a67e79be
Lars Magnus Nordeide <600871@stud.hvl.no>
2024-10-16 11:46:10 +0200

    Init Server: Spring Boot

---
+++ server/src/main/java/no/hvl/dat250/g13/project/ServerApplication.java
@@ -0,0 +1,13 @@
+package no.hvl.dat250.g13.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
