f3db507a9605461953f5b2e1109ffe81208d84e8
Lars Magnus Nordeide <600871@stud.hvl.no>
2024-11-14 04:59:46 +0100

    Refactor ResultTest

--- server/src/test/java/no/hvl/dat250/g13/project/util/ResultTest.java
+++ server/src/test/java/no/hvl/dat250/g13/project/util/ResultTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ResultTest {
 
-    private static final Result<String, String> ok = new Result.Ok<>("value");
+    private static final Result<String, String> ok = new Result.Ok<>("id");
     private static final Result<String, String> error = new Result.Error<>("error");
 
     @Test
@@ -25,7 +25,7 @@ class ResultTest {
 
     @Test
     void getOk() {
-        assertEquals(Optional.of("value"), ok.getOk());
+        assertEquals(Optional.of("id"), ok.getOk());
         assertEquals(Optional.empty(), error.getOk());
     }
 
@@ -37,7 +37,7 @@ class ResultTest {
 
     @Test
     void value() {
-        assertEquals("value", ok.value());
+        assertEquals("id", ok.value());
         assertThrows(UnsupportedOperationException.class, error::value);
     }
 
